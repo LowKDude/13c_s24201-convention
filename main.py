4c3b86deea71df5c24de5b9f3e98a1a0a047e378
s24201 <s24201@pjwstk.edu.pl>
2024-03-24 15:56:06 +0100

    Task 9: Function Overriding

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from square_generator.square_generator import SquareGenerator
 from square_generator.cubic_generator import CubicGenerator
-
+try:
+    cubic_gen = CubicGenerator()
+    squares = cubic_gen.generate_squares(10, 1)
+    print("Squares:", squares)
+except Exception as e:
+    print(f"Error: {e}")
 cubic_gen = CubicGenerator()
 cubes = cubic_gen.generate_squares(1, 5)
 print("Cubes:", cubes)

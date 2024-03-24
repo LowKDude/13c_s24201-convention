df91d1cfac602e075d965525d4a6d5788d0b1654
s24201 <s24201@pjwstk.edu.pl>
2024-03-24 14:35:17 +0100

    Task 4: Libraries

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import math
+
+
 class SquareGenerator:
     def generate_squares(self, start, end):
 
-        return [x**2 for x in range(start, end + 1)]
+        return [x ** 2 for x in range(start, end + 1)]
+
+    def square_roots(self, numbers):
+
+        return [math.sqrt(number) for number in numbers]
+
 
 # Example usage
 square_gen = SquareGenerator()
 squares = square_gen.generate_squares(1, 10)
-print(squares)
+print("Squares:", squares)
+
+square_roots = square_gen.square_roots(squares)
+print("Square Roots:", square_roots)
